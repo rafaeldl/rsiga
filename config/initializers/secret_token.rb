790e2dc8b9cadc97d8b8ecbf5d0f8814c856ce78
rafaeldl <rafaeldll@gmail.com>
2012-09-13 20:48:12 -0300

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rsiga::Application.config.secret_token = '6aedc161ed610b848d1957a23404f4d9b91b0778203186fccf9bad0ac91fedb722eb2298c8f7b5e05d2f0d875cfeb85d4b7d99ddce228eb4849d1800eccd4473'
